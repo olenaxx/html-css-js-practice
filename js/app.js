198933031dee0e5b5f15a043cf2aa6e481e20a12
Olena Stadnyk <olena.lightness@gmail.com>
2022-05-19 13:44:25 +0300

    feat: add scroll reveal

--- js/app.js
+++ js/app.js
@@ -9,3 +9,13 @@ function stickyNavbar() {
 stickyNavbar();
 
 window.addEventListener('scroll', stickyNavbar)
+
+/* ------------------ Reveal Animation -------------------- */
+
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: '60px',
+});
+
+sr.reveal('.showcase-info', {delay: 600})
+sr.reveal('.showcase-image', {origin: 'top', delay: 700})
